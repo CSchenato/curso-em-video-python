9fe920389f7ef8aa95e524c74abd8382ddd318f9
Camila Schenato <camilaschenato97@gmail.com>
2022-11-29 19:49:02 -0300

    Exercício Python 007
    # Desafio 007 - Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média

---
+++ ex007.py
@@ -0,0 +1,7 @@
+# Desafio 007 - Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+print(f'A nota 1 foi: {n1}. A nota 2 foi: {n2}. Sua média é: {(n1+n2)/2:.2f}')
+
+# Na resolução o professor colocou uma linha a mais com a média, mas preferi deixar tudo em uma só.
